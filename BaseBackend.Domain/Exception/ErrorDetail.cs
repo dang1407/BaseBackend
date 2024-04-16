0aaafd77fa474e56f6b2dd3062a58bd2d34177b3
Minh Dang <dang14072k2@gmail.com>
2024-04-16 21:47:53 +0700

    Add project files.

---
+++ BaseBackend.Domain/Exception/ErrorDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseBackend.Domain
+{
+    public class ErrorDetail
+    {
+        public string FieldName { get; set; } = string.Empty;
+        public string ErrorMessage { get; set; } = string.Empty;
+    }
+}
